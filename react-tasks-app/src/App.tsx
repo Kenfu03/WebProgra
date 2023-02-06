747357a50430e82495bf2e0868bf765c321f94f0
LaMenopausia <kennethesa324@gmail.com>
2023-02-06 12:06:17 -0600

    update tasks

--- react-tasks-app/src/App.tsx
+++ react-tasks-app/src/App.tsx
@@ -1,24 +1,27 @@
 import { TaskApp, Calculator, SearchTree } from "./containers";
 import { DivEnclouser } from "./components";
+import hojas from "./assets/img/hojas.jpg";
 import "./App.css";
 
 function App(): JSX.Element {
   return (
-    <div className="AppContainer">
-      <div className="first childs">
-        <DivEnclouser>
-          <TaskApp></TaskApp>
-        </DivEnclouser>
-      </div>
-      <div className="second childs">
-        <DivEnclouser>
-          <Calculator></Calculator>
-        </DivEnclouser>
-      </div>
-      <div className="third childs">
-        <DivEnclouser>
-          <SearchTree></SearchTree>
-        </DivEnclouser>
+    <div className="BackgroundImage">
+      <div className="AppContainer">
+        <div className="first childs">
+          <DivEnclouser>
+            <TaskApp></TaskApp>
+          </DivEnclouser>
+        </div>
+        <div className="second childs">
+          <DivEnclouser>
+            <Calculator></Calculator>
+          </DivEnclouser>
+        </div>
+        <div className="third childs">
+          <DivEnclouser>
+            <SearchTree></SearchTree>
+          </DivEnclouser>
+        </div>
       </div>
     </div>
   );
