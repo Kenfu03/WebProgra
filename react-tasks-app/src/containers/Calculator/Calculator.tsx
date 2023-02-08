e4877e802b2350adf74ff159f2ccd8218246fe86
LaMenopausia <kennethesa324@gmail.com>
2023-02-07 23:20:55 -0600

    calculator

--- react-tasks-app/src/containers/Calculator/Calculator.tsx
+++ react-tasks-app/src/containers/Calculator/Calculator.tsx
@@ -5,22 +5,27 @@ import "./CalcButton/CalcButton.css";
 interface buttonsTypers {
   tipo: string;
   valor: string;
+  color?: string;
 }
 
 export const Calculator = () => {
-  const [resFlag, setResFlag] = useState(true);
+  const [ChangeColor, setChangeColor] = useState(false);
   const [firstVal, setFirstVal] = useState("");
   const [ope, setOpe] = useState("");
   const [value, setValue] = useState("");
 
+  const defaultColor:string = "#1d3126";
+
   const calcFunctions = (valor: string) => {
     if (valor === "AC") {
+      setChangeColor(false);
       setValue("");
     } else if (valor === "CE") {
       value.length >= 1 && setValue(value.slice(0, -1));
     } else {
       if (value === ""){
         setValue("Faltan Datos")
+        
       }
       else if (firstVal === ""){
         setFirstVal(value)
@@ -35,9 +40,15 @@ export const Calculator = () => {
   };
 
   const calcOperations = (valor: string) => {
-    setOpe(valor);
-    setFirstVal(value);
-    setValue("");
+    setChangeColor(!ChangeColor)
+    if (ope === "") {
+      setOpe(valor);
+      setFirstVal(value);
+      setValue("");
+    } else {
+      setOpe(valor);
+    }
+    
   };
 
   const calc = (tipo: string, valor: string) => {
@@ -46,17 +57,20 @@ export const Calculator = () => {
     } else if (tipo === "operacion") {
       calcOperations(valor);
     } else {
-      setValue(value + valor);
+      if (value === "Faltan Datos") {
+        setValue(valor)
+      } else {
+        setValue(value + valor);
+      }
     }
   };
 
   function CalcButton(props: buttonsTypers) {
     return (
       <button
-        className={props.tipo}
+        className={ChangeColor? "operacion2":props.tipo }
         onClick={() => {
           calc(props.tipo, props.valor)
-           
           }}
       >
         {props.valor}
@@ -77,20 +91,20 @@ export const Calculator = () => {
         <div className="calc-button_container">
           <CalcButton tipo="funcion" valor="AC"></CalcButton>
           <CalcButton tipo="funcion" valor="CE"></CalcButton>
-          <CalcButton tipo="operacion" valor="%"></CalcButton>
-          <CalcButton tipo="operacion" valor="÷"></CalcButton>
+          <CalcButton tipo="operacion" valor="%" color="white"></CalcButton>
+          <CalcButton tipo="operacion" valor="/" color="white"></CalcButton>
           <CalcButton tipo="numero" valor="7"></CalcButton>
           <CalcButton tipo="numero" valor="8"></CalcButton>
           <CalcButton tipo="numero" valor="9"></CalcButton>
-          <CalcButton tipo="operacion" valor="*"></CalcButton>
+          <CalcButton tipo="operacion" valor="*" color="white"></CalcButton>
           <CalcButton tipo="numero" valor="4"></CalcButton>
           <CalcButton tipo="numero" valor="5"></CalcButton>
           <CalcButton tipo="numero" valor="6"></CalcButton>
-          <CalcButton tipo="operacion" valor="-"></CalcButton>
+          <CalcButton tipo="operacion" valor="-" color="white"></CalcButton>
           <CalcButton tipo="numero" valor="1"></CalcButton>
           <CalcButton tipo="numero" valor="2"></CalcButton>
           <CalcButton tipo="numero" valor="3"></CalcButton>
-          <CalcButton tipo="operacion" valor="+"></CalcButton>
+          <CalcButton tipo="operacion" valor="+" color="white"></CalcButton>
           <CalcButton tipo="numero2" valor="0"></CalcButton>
           <CalcButton tipo="numero" valor="."></CalcButton>
           <CalcButton tipo="funcion" valor="="></CalcButton>
