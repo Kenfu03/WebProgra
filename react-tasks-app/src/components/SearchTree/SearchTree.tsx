973cde931cf4ec0575c1c0f7cfa7caf536eec87c
LaMenopausia <kennethesa324@gmail.com>
2023-02-15 12:03:33 -0600

    searchTree update

--- react-tasks-app/src/components/SearchTree/SearchTree.tsx
+++ react-tasks-app/src/components/SearchTree/SearchTree.tsx
@@ -1,52 +1,72 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import "./SearchTree.css";
+import { DecorationDiv } from "../../containers";
 
 type FormElement = React.FormEvent<HTMLFormElement>;
 
+interface iCategorys {
+  name: string;
+}
+
 export const SearchTree = () => {
+  const taskInput = useRef<HTMLInputElement>(null);
+  
   const [switchInfo, setSwitchInfo] = useState(false);
-  const [newEmpresa, setNewEmpresa] = useState("")
+  const [newCategory, setNewCategory] = useState("");
+  const [categorys, setCategorys] = useState<iCategorys[]>([]);
 
   const changeData = () => {
     setSwitchInfo(!switchInfo);
   };
 
-  const handleSubmit = (e: FormElement) => {
+  const addE = (e: FormElement) => {
+    console.log(newCategory);
     e.preventDefault();
-    // addTask(newEmpresa);
-    setNewEmpresa("");
+    addCategory(newCategory);
+    setNewCategory("");
   };
 
-  // const addTask = (name: string) => {
-  //   const newTasks: iTask[] = [...tasks, { name, done: false }];
-  //   setTasks(newTasks);
-  // };
-
-  const addDatos = () => {
-    return <div className="addDatos-container">
-      <input type="text" placeholder="Empresa"/>
-      <button>Agregar Empresa</button>
-    </div>;
+  const addCategory = (name: string) => {
+    const newCategorys: iCategorys[] = [...categorys, { name }];
+    setCategorys(newCategorys);
   };
 
-  const infoTree = () => {
+  const addDatosContainer = () => {
+    return (
+      <div className="addDatos-container">
+        <form onSubmit={addE}>
+          <input type="text" onChange={(e) => setNewCategory(e.target.value)} value={newCategory} />
+          <button onClick={() => addE}>Agregar Category</button>
+        </form>
+      </div>
+    );
+  };
+
+  const infoTreeContainer = () => {
     return (
       <div className="infoTree-container">
-        <h1>me kago en todo</h1>
+        {categorys.map((t: iCategorys, i: number) => (
+          <div key={i}>
+            <button>
+              <DecorationDiv firstCo="#a88c43" />
+              <h1>{t.name}</h1>
+            </button>
+          </div>
+        ))}
       </div>
-    )
-  }
+    );
+  };
 
   return (
     <div className="SearchTree-container">
-      <button
+      <button className="ChangeDiv"
         onClick={() => {
           changeData();
         }}
       >
         {switchInfo ? "Agregar Items" : "Arbol de busqueda"}
       </button>
-      {switchInfo ? addDatos() : infoTree()}
+      {switchInfo ? addDatosContainer() : infoTreeContainer()}
     </div>
   );
 };
